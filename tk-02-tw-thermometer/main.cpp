df3365ad2bd970433edad3cce2e1fbda4c77e9b4
codecoolmz <vagasiferi4ever@gmail.com>
2017-02-22 09:23:07 +0100

    temp modification

--- tk-02-tw-thermometer/main.cpp
+++ tk-02-tw-thermometer/main.cpp
@@ -5,7 +5,9 @@
 #include "codecool/codecool_pwm.h"
 #include "codecool/codecool_joystick.h"
 #include "codecool/codecool_shield_names.h"
-#include "codecool/codecool_pwm.h"
+#include "codecool/codecool_pot.h"
+#include "temp_modify.h"
+
 
 #define freq 2000;
 #define LM75_ADDRESS 0x90
@@ -24,7 +26,6 @@ int main() {
     LCD_CLS();
     LCD_LOCATE(0, 0);
     LCD_PRINTF("PUSH THE BUTTON FOR SENDING");
-
     wait(2);
 
     uint8_t buffer_temp[16];
@@ -32,7 +33,7 @@ int main() {
 
     while (true) {
 
-        if(JOYSTICK_PUSHED){
+        /*if(JOYSTICK_PUSHED){
         	PWM_FREQUENCY(SPEAKER, 0);
         	PWM_SET_PULSE_WIDTH(SPEAKER, 0);
         	LCD_CLS();
@@ -43,22 +44,32 @@ int main() {
             I2C_READ(LM75_ADDRESS, buffer_temp, 2);
             int8_t _int_part = (int8_t)buffer_temp[0];
             temp = _int_part + 0.5f * ((buffer_temp[1]&0x80)>>7);
+            //temp = temp + temp_modify(POT1_READ);
     		LCD_CLS();
             LCD_LOCATE(0,0);
             LCD_PRINTF("sent: %.1f", temp);
             SERIAL_SEND(buffer_temp, 16);
         }
+        LCD_LOCATE(0,10);
+        LCD_PRINTF("Current temperature: %.1f", temp);
+        LCD_LOCATE(0,20);
+		LCD_PRINTF("Modified temp: %.1f", temp + temp_send_modify(POT1_READ));*/
 
-        if(JOYSTICK_LEFT){
+
+
+        if(JOYSTICK_PUSHED){
+        	LCD_CLS();
 			memset(buffer_temp, 0x00, sizeof(buffer_temp));
 			SERIAL_RECV(buffer_temp, 16);
 			float tempi;
 			int8_t _int_part = (int8_t)buffer_temp[0];
-			tempi = _int_part + 0.5f * ((buffer_temp[1]&0x80)>>7);
-			LCD_LOCATE(0,12);
+			tempi = _int_part+ temp_recieve_modify(POT2_READ) + 0.5f * ((buffer_temp[1]&0x80)>>7);
+			LCD_LOCATE(0,0);
 			LCD_PRINTF("Received: %.1f", tempi);
+			LCD_LOCATE(0,12);
+			LCD_PRINTF("Received: %.1f", temp_recieve_modify(POT2_READ));
 
-			if(temp < 10){
+			if(tempi < 10){
 				PWM_SET_PULSE_WIDTH(LED_RED_SHIELD, 0);
 				PWM_SET_PULSE_WIDTH(LED_GREEN_SHIELD, 0);
 				PWM_SET_PULSE_WIDTH(LED_BLUE_SHIELD, 100);
@@ -67,7 +78,7 @@ int main() {
 			}
 
 
-			if(tempi >= 10 && temp < 18){
+			if(tempi >= 10 && tempi < 18){
 				PWM_SET_PULSE_WIDTH(LED_RED_SHIELD, 0);
 				PWM_SET_PULSE_WIDTH(LED_GREEN_SHIELD, 100);
 				PWM_SET_PULSE_WIDTH(LED_BLUE_SHIELD, 100);
@@ -75,7 +86,7 @@ int main() {
 				PWM_SET_PULSE_WIDTH(SPEAKER, 1);
 			}
 
-			if(tempi >= 18 && temp < 26){
+			if(tempi >= 18 && tempi < 26){
 				PWM_SET_PULSE_WIDTH(LED_RED_SHIELD, 0);
 				PWM_SET_PULSE_WIDTH(LED_GREEN_SHIELD, 100);
 				PWM_SET_PULSE_WIDTH(LED_BLUE_SHIELD, 0);
@@ -84,7 +95,7 @@ int main() {
 			}
 
 
-			if(tempi >= 26 && temp < 34){
+			if(tempi >= 26 && tempi < 34){
 				PWM_SET_PULSE_WIDTH(LED_RED_SHIELD, 100);
 				PWM_SET_PULSE_WIDTH(LED_GREEN_SHIELD, 100);
 				PWM_SET_PULSE_WIDTH(LED_BLUE_SHIELD, 0);
@@ -101,7 +112,7 @@ int main() {
 				PWM_FREQUENCY(SPEAKER, 1000);
 				PWM_SET_PULSE_WIDTH(SPEAKER, 1);
 			}
-			wait(2);
-}
+			wait(0.2);
+        }
     }
 }
---
+++ tk-02-tw-thermometer/temp_modify.h
@@ -0,0 +1,19 @@
+/*
+ * temp_modify.h
+ *
+ *  Created on: Feb 21, 2017
+ *      Author: mz
+ */
+
+#ifndef CODECOOL_TEMP_MODIFY_H_
+#define CODECOOL_TEMP_MODIFY_H_
+
+float temp_send_modify(float x){
+	return (x-0.5)*20;
+}
+
+float temp_recieve_modify(float x){
+	return (x-0.5)*10;
+}
+
+#endif /* CODECOOL_TEMP_MODIFY_H_ */
